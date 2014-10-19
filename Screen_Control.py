e2064d42ad570168177f6602a21c92194518683b
Kravlin <Kravlin@gmail.com>
2014-10-19 01:33:13 -0600

    Updated to use writeLCD instead of updateScreen. This combines the screen color change and the text update at the same time

--- Screen_Control.py
+++ Screen_Control.py
@@ -7,8 +7,16 @@ import cups
 from os import listdir, remove
 from os.path import isfile, join
 import RPi.GPIO as GPIO
+from enum import Enum
 
-class stickerPrinter:
+
+class Color(Enum):
+	red = 1
+	blue = 2
+	green = 3
+	white = 4
+
+class StickerPrinter:
 
 	# Class Variables! YAY!
 	# should flesh these out more, possibly add some protection
@@ -20,17 +28,13 @@ class stickerPrinter:
 	printer = None
 		
 	# what's called when we make the stickerprinter. I'd like to make this less....hardcoded.
-	# As well, there's a bunch of stuff between this and class variable that's no longer used that we should nix.
+	
 	def __init__(self, imageLocation):
 		self.imageLocation = imageLocation
 		self.lcd = Adafruit_CharLCDPlate()
-		self.lcd.message("Initializing\nStickerBOT9000")
+		self.writeLCD("Initializing\nStickerbot9001",Color.white)
 		self.conn = cups.Connection()
 		self.printer = self.conn.getPrinters().keys()[0]
-		self.buttonPin=4
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(self.buttonPin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-		GPIO.add_event_detect(4, GPIO.RISING, bouncetime=3000)
 
 	# This originally handled all the code for getting a dollar, but was slowly reduced down and now it does literally fuck-all
 	# However, once we get the abiliy to control the bill acceptor, and make this much more robust,
@@ -41,25 +45,20 @@ class stickerPrinter:
 
 	# This was written as a way to easily update the LCD screen, since we do it so goddamn much.
 	# Should be combined with the screen color method at the bottom of this file, because we change colors so much too.
-	def updateScreen(self, text):
-		self.lcd.clear()
-		self.lcd.message("Image: "+str(self.currentID)+"\n"+text)
 
 	# This method handles all of the logic for actually getting a sticker from "selected" to printed.
 	def printFile(self):
-		self.updateScreen("Now Printing")
-		self.lcd.backlight(self.lcd.RED)
+		self.writeLCD("Now Printing",Color.red)
 		image = self.imageLocation+self.files[self.currentID]
 		print "printing: "+image
 		# Could use a much better descriptor here than "Python_Status_print"
-		printid = self.conn.printFile(self.printer, image, "Python_Status_print", {})
+		printid = self.conn.printFile(self.printer, image, "StickerBot Sticker: "+str(self.currentID), {})
 		# Here we do nothing until the job is done. Literally a loop until the printer no longer has the job in it's queue.
 		# Possible issue here. If there are other hung jobs in the printer, the printer can just sit there and the entire
 		# bot can hang. should look into wiping out all jobs in queue at the start of stickerbot maybe?	
 		while(self.conn.getJobs().get(printid,None) is not None):
 			pass
-		self.updateScreen("\nPrinted. Thanks!")
-		self.lcd.backlight(self.lcd.GREEN)
+		self.writeLCD("\nPrinted. Thanks!",Color.green)
 		# This sleep is here purely so that the user gets a chance to see the message.
 		sleep(3)
 		# This was implemented at defcon in order to provide the standard stickers all the time.
@@ -120,38 +119,39 @@ class stickerPrinter:
 
 	# this is the main loop of stickerbot. Running this method outside of a stickerbot object puts it in an indefinite loop.
 	def run(self):
-		self.lcd.clear()
-		self.lcd.message("Ready")
+		writeLCD("Ready", Color.white)
 		self.checkFiles()
 		if len(list(self.files.viewkeys())) != 0:	
 			self.currentID = int(list(self.files.viewkeys())[0])
 		else:
 			self.currentID = 0
 		print "THE FIRST USER IS LUCKY NUMBER: "+str(self.currentID)
-		self.updateScreen("Please Insert $1")
+		self.writeLCD("Please Insert $1",Color.white)
 		while True:
 			if self.currentID != 0:
 				if (self.checkInput()):
-					self.updateScreen("Please Insert $1")
+					self.writeLCD("Please Insert $1",Color.white)
 			else:
-				self.lcd.clear()
-				self.lcd.message("Ready")
+				self.writeLCD("Ready",Color.white)
 				while len(list(self.files.viewkeys())) == 0:
 					sleep(.5)
 					self.checkFiles()
 				self.getNextID()
-				self.updateScreen("Please Insert $1")	
-			self.lcd.backlight(self.lcd.ON)
+				self.writeLCD("Image: "+self.currentID+"\nPlease Insert $1",Color.red)
 			self.checkFiles()
 
 #This code is currently useless, but was intended originally to make setting the screen color easier. Should probably be merged with code printing from the LCD.
-	def colorScreen(self, color):
-		if(color == "RED"):
+	def writeLCD(self, inputText, color):
+		if(color == Color.red):
 			self.lcd.backlight(self.lcd.RED)
-		if(color == "GREEN"):
+		if(color == Color.green):
 			self.lcd.backlight(self.lcd.GREEN)
-		if(color == "WHITE"):
+		if(color == Color.white):
 			self.lcd.backlight(self.lcd.WHITE)
+		if(color == Color.blue):
+			self.lcd.backlight(self.lcd.BLUE)
+		self.lcd.clear()
+		self.lcd.message(inputText)
 
 #If this is called instead of used as a class, we create an object and run it.
 #This needs a ton of work with argparse and awesome stuff.
@@ -163,7 +163,7 @@ class stickerPrinter:
 	# 5. Profit.
 if __name__ == "__main__":
 	images = "/srv/http/images/"
-	printStickers = stickerPrinter(images)
+	printStickers = StickerPrinter(images)
 	printStickers.checkFiles()
 	sleep(3)
 	printStickers.run()
